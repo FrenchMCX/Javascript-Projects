420a080683c1d022886c9bd57cc6f5df789705e6
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-04-30 16:10:39 -0400

    pet project

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,15 @@
+//Create function describing my newest pet
+function my_Dictionary() {
+    var NewPet = {
+        Species:"Cat",
+        Color:"Black&Brown",
+        Breed:"American Shorthair",
+        Age:8,
+        Name: "Monkeh",
+        Sound: "Meow"
+    };
+    //Delete the sound before it's displayed
+    delete Animal.Sound;
+    //Display the now deleted Sound
+    document.getElementById("Dictionary").innerHTML = NewPet.Sound;
+}
