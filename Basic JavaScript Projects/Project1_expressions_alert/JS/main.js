fc4287a0758f98ac138a678fb7b29db706a9347e
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-04-30 12:15:03 -0400

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,20 @@
+//Display window alert saying "Hello World"
 window.alert("Hello, World!");
-document.write(A);
+// Display strings
+document.write("These are string\'s");
+document.write("\"Positive anything is better than negative nothing.\" - Elbert Hubbard");
+//Declare variables A and B
 var A = "This is a string";
-var string = "This is a string";
+var B = "\"Positive anything is better than negative nothing.\" -Elbert Hubbard" + "This is a string";
+//Display variables A and B
+document.write(A);
+document.write(B);
+//Declare variables Family, Dad, Mom, Daughters, and Son.
+var Family = "The French's", Dad = "John", Mom = "Ana", Daughters= "Allie and Gabby", Son = "Tyler";
+//Display Dad
+document.write(Dad);
+//Display an expression
+document.write(40+45);
+
+//Set font color
+document.write(A.fontcolor("blue"));
